f302d911ae33555835ad38e400eaf8118c9023e8
Meng Dong <15093138420@163.com>
2014-09-16 15:44:43 +0800

    Initial Commit

---
+++ KuAiVideo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KuAiVideo
+//
+//  Created by mengdong on 14-9-16.
+//  Copyright (c) 2014年 com.mengdongliuyaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
